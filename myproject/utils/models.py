ac4e562b79915a37f40c5850115c0902fbd5eade
Gustavo Griffo <gustavogriffo@gustavos-mbp.attlocal.net>
2018-05-16 23:34:51 -0500

    aditional files

---
+++ myproject/utils/models.py
@@ -0,0 +1,22 @@
+# Create your models here.
+from __future__ import unicode_literals import urlparse
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
+from utils.models import UrlMixin
+from utils.models import CreationModificationMixin
+from utils.models import MetaTagsMixin
+from django.contrib.sites.models import Site
+from django.config import settings
+
+@python_2_unicode_compatible
+class Idea(UrlMixin, CreationModificationMixin, MetaTagsMixin):
+    title = models.CharField(_("Title"), max_length = 200)
+    content = models.CharField(_("Content"))
+
+class Meta:
+    verbose_name = _("Idea")
+    verbose_name_plural = _("Ideas")
+
+def __str__(self):
+    return self.title
